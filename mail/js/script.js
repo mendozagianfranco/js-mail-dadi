79f1dcafd0cffd55a833eea9d3c85d95c5b6d43f
mendozagianfranco <mendozagianfranco88@gmail.com>
2025-02-27 15:34:36 +0100

    feat: add loop and control list mails

--- mail/js/script.js
+++ mail/js/script.js
@@ -1,5 +1,21 @@
 // Array con le email degli invitati
-const guestMails = ['mail1', 'mail2', 'mail3', 'mail4',];
+const guestMails = ['mail 1', 'mail 2', 'mail 3', 'mail 4',];
 
 //  Chiedere all'utente la sua email e salvare in una varibiale
 const mailUser = prompt('Inserire la tua email');
+
+// Usare Ciclo per controllare la lista e trovare corrispondenza con email fornita dall'utente
+
+for (let i = 0; i < guestMails.length; i++) {
+    let currentMail = guestMails[i];
+
+    // Stampare in base alla corrispondeza
+    if (currentMail === mailUser) {
+        console.log('Sei tra gli invitati alla festa');
+        break;
+    } else {
+        console.log('Non sei tra gli invitati alla festa');
+        break;
+    }
+
+}
