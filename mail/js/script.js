625b0bcb6d7e74f2b62b1f797f059ea069c8b46c
mendozagianfranco <mendozagianfranco88@gmail.com>
2025-02-27 15:38:43 +0100

    feat: add control input user

--- mail/js/script.js
+++ mail/js/script.js
@@ -6,16 +6,19 @@ const mailUser = prompt('Inserire la tua email');
 
 // Usare Ciclo per controllare la lista e trovare corrispondenza con email fornita dall'utente
 
-for (let i = 0; i < guestMails.length; i++) {
-    let currentMail = guestMails[i];
+if (mailUser) {
+    for (let i = 0; i < guestMails.length; i++) {
+        let currentMail = guestMails[i];
 
-    // Stampare in base alla corrispondeza
-    if (currentMail === mailUser) {
-        console.log('Sei tra gli invitati alla festa');
-        break;
-    } else {
-        console.log('Non sei tra gli invitati alla festa');
-        break;
+        // Stampare in base alla corrispondeza
+        if (currentMail === mailUser) {
+            console.log('Sei tra gli invitati alla festa');
+            break;
+        } else {
+            console.log('Non sei tra gli invitati alla festa');
+            break;
+        }
     }
-
+} else {
+    console.log('Non lasciare il campo vuoto');
 }
