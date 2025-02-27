e0addae0296b0ab91fec2d8ec4c0ba3a2554da2e
mendozagianfranco <mendozagianfranco88@gmail.com>
2025-02-27 15:13:16 +0100

    feat: create array and prompt for user

---
+++ mail/js/script.js
@@ -0,0 +1,5 @@
+// Array con le email degli invitati
+const guestMails = ['mail1', 'mail2', 'mail3', 'mail4',];
+
+//  Chiedere all'utente la sua email e salvare in una varibiale
+const mailUser = prompt('Inserire la tua email');
